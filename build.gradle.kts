2103f554a2b9219eb0b883cf73eb4f7afe2a55ad
KonstantyBialek <konstanty.bialek@student.wsb.edu.pl>
2024-03-18 21:52:40 +0100

    Updated some dependencies for plugins and performed dependency cleanup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,12 +5,12 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.1")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.38.1")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.1")
     }
 }
 
 plugins {
-    id("com.android.application") version "8.3.0" apply false
+    id("com.android.application") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
