0c2980f8ab48905bc5c9a6d95e3e13bb14f119b1
KonstantyBialek <konstanty.bialek@student.wsb.edu.pl>
2024-03-18 22:54:45 +0100

    1. Began implementing unit tests for the application's ParkingSlotRepository. 2. Added Mockk libraries for testing purposes in Kotlin application.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,16 +71,19 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("com.google.firebase:firebase-firestore-ktx")
     implementation("com.google.dagger:hilt-android:2.51")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk:1.12.0")
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.02"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    androidTestImplementation("io.mockk:mockk-android:1.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
---
+++ app/src/test/java/com/parkingmanagerapp/repository/ParkingSlotRepositoryTest.kt
@@ -0,0 +1,33 @@
+package com.parkingmanagerapp.repository
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class ParkingSlotRepositoryTest {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun addParkingSlot() {
+    }
+
+    @Test
+    fun getAllParkingSlots() {
+    }
+
+    @Test
+    fun getAllReservations() {
+    }
+
+    @Test
+    fun getUserReservations() {
+    }
+
+    @Test
+    fun cancelReservation() {
+    }
+}
