9539a08d4f89e53bcfa0e1ec7a41039a566be9e8
KonstantyBialek <konstanty.bialek@student.wsb.edu.pl>
2024-03-26 12:09:50 +0100

    1. Added new provider to the Hilt for Firebase Authentication. 2. Laid out some basic classes for future development of database functions and tests. 3. Added basic functionalities to the UserRepository class.

--- app/src/main/java/com/parkingmanagerapp/model/User.kt
+++ app/src/main/java/com/parkingmanagerapp/model/User.kt
@@ -3,7 +3,23 @@ package com.parkingmanagerapp.model
 data class User(
     val name: String,
     val surname: String,
-    val phoneNumber: Number,
+    val phoneNumber: String,
     val email: String,
+    val role: UserRole,
     val reservations: List<Reservation>
-)
+) {
+    fun isAdmin() = role == UserRole.ADMIN
+
+    companion object {
+        fun fromFirebaseUser(firebaseUser: com.google.firebase.auth.FirebaseUser, role: UserRole = UserRole.REGULAR): User {
+            return User(
+                name = firebaseUser.displayName ?: "",
+                surname = "",
+                phoneNumber = firebaseUser.phoneNumber ?: "",
+                email = firebaseUser.email ?: "",
+                role = role,
+                reservations = emptyList()
+            )
+        }
+    }
+}
