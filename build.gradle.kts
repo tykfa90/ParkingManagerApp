67c0f94f62fab4e3897cfff4ab8f78b748648346
KonstantyBialek <konstanty.bialek@student.wsb.edu.pl>
2024-11-08 17:00:35 +0100

    1. Updated the dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     }
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.23-1.0.19")
     }
 }
@@ -14,7 +14,7 @@ plugins {
     id("com.android.application") version "8.3.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-    id("com.google.dagger.hilt.android") version "2.51" apply false
+    id("com.google.dagger.hilt.android") version "2.52" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
     id("de.mannodermaus.android-junit5") version "1.10.0.0" apply false
 }
